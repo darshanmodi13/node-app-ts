de7cb5aa46131b6447a203caa6572f1375bd17e8
darshanmodi13 <darshan@nextcampus.in>
2023-09-17 17:17:00 +0530

    added swagger support

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,19 @@ import { MONGO_URI, PORT, NODE_ENV } from "@config/env";
 // Dependencies
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
-import { useExpressServer } from "routing-controllers";
+import { useExpressServer, getMetadataArgsStorage } from "routing-controllers";
+import helmet from "helmet";
+import hpp from "hpp";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Database
 import MongoDB from "@providers/mongoDB.provider";
+
+// Swagger
+import { routingControllersToSpec } from "routing-controllers-openapi";
+import swaggerUI from "swagger-ui-express";
 
 // Base Error Class
 import BaseError from "@/errors/BaseError";
@@ -26,6 +35,7 @@ export class App {
     this.initializeDB();
     this.initializeMiddleWare();
     this.initializeRoutes();
+    this.initializeSwagger();
     this.notListedRoutesHandler();
     this.initializeErrorHandler();
   }
@@ -40,17 +50,30 @@ export class App {
         __dirname + "/controllers/**/*.controller.js",
       ],
       defaultErrorHandler: false,
+      routePrefix: "/api",
     });
   }
   private initializeMiddleWare() {
     this.app.use(morgan("tiny"));
+    this.app.use(cors());
+    this.app.use(hpp());
+    this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(express.json({ limit: "50mb" }));
+    this.app.use(
+      express.urlencoded({
+        limit: "50mb",
+        extended: true,
+        parameterLimit: 50000,
+      })
+    );
+    this.app.use(cookieParser());
   }
   private notListedRoutesHandler() {
     this.app.all("*", (req, res) => {
       if (!res.headersSent) throw new NotFoundError("Route Not Found");
     });
   }
-
   private initializeErrorHandler() {
     this.app.use(
       (err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -64,6 +87,23 @@ export class App {
         return res.status(503).send("Service Unavailable");
       }
     );
+  }
+  private async initializeSwagger() {
+    const metadataArgsStorage = getMetadataArgsStorage();
+    // Generate the OpenAPI specification
+    const spec = routingControllersToSpec(metadataArgsStorage);
+    const swaggerSpec = {
+      ...spec,
+      info: { ...spec.info, title: "Documentation" },
+      servers: [
+        {
+          url: `http://localhost:${this.port}/api`,
+        },
+      ],
+    };
+    console.log(swaggerSpec);
+    this.app.use("/api/docs", swaggerUI.serve);
+    this.app.get("/api/docs", swaggerUI.setup(swaggerSpec));
   }
 
   public listen() {
--- src/controllers/health-check.controller.ts
+++ src/controllers/health-check.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from "routing-controllers";
+import { JsonController, Get, Req, Res } from "routing-controllers";
 import { Request, Response } from "express";
 
 // Generic Class
@@ -7,17 +7,20 @@ import GenericResponse from "@/utils/GenericResponse";
 // Response
 import StatusCodes from "@/utils/httpResponseCode";
 
+// Swagger
+import { OpenAPI } from "routing-controllers-openapi";
 enum Routes {
   ROOT = "/_",
   HEALTH = "/health",
 }
 
-@Controller(Routes.ROOT)
+@JsonController(Routes.ROOT)
 class HealthCheck {
   @Get(Routes.HEALTH)
+  @OpenAPI({ description: "Health Check" })
   health(@Req() req: Request, @Res() res: Response) {
     const response = new GenericResponse({ message: "Hey There!!" });
-    return res.status(StatusCodes.OK).json({ message: "Hey There..." });
+    return res.status(StatusCodes.OK).json(response.getBody());
   }
 }
 
